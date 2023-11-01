322329533517608a37bfe15e233f1d9745adb59b
ironman-eric <eonno123@yahoo.com>
2023-11-01 14:08:47 -0700

    clean up and readme

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # thrive_challenge
-thrive interview challenge
+Small ruby file that consolidates user and company json into our desired file format
+
+## Table of Contents
+
+- [General Info](#general-information)
+- [Technologies Used](#technologies-used)
+- [Features](#features)
+- [Setup](#setup)
+- [Usage](#usage)
+
+## General Information
+
+Small ruby file that consolidates user and company json into our desired file format
+
+## Technologies Used
+
+- ruby
+
+## Features
+
+- Reads user and company json
+- Filters and groups users based on the company they belong to and their email status
+- Reshapes and writes data to a text file
+
+See [challenge.txt](./challenge.txt) for more details
+
+## Setup
+
+- install ruby
+
+## Usage
+
+```
+ruby .\challenge.rb
+```
--- challenge.rb
+++ challenge.rb
@@ -1,74 +1,101 @@
 require 'json'
 
+# Sort array of users by last name
+#
+# @param [Array, #users] users to be sorted
+# @return [Array, #users] sorted users
 def order_users_by_last_name(users)
   users.sort_by { |user| user['last_name'] }
 end
 
+# Sort array of companies by id
+#
+# @param [Array, #companies] companies to be sorted
+# @return [Array, #companies] sorted companies
 def order_companies_by_id(companies)
   companies.sort_by { |company| company['id'] }
 end
 
+# filter and group our users for a particular company
+#
+# @param [Array, #users] users to be filtered and grouped
+# @param [Object, #company] company specific info
+# @return [Array, #users] filtered and grouped users by status
 def group_users_by_company(users, company)
   users
     .select { |user| user['company_id'] == company['id'] && user['active_status'] == true }
     .map { |user| user.merge!({ 'tokens_topped_up' => user['tokens'] + company['top_up'] }) }
-    .group_by { |user| user['email_status'] ? :emailed : :not_emailed }
+    .group_by { |user| company['email_status'] && user['email_status'] ? :emailed : :not_emailed }
 end
 
+# sum all the tops ups for a given company
+#
+# @param [Array, #users] users
+# @param [Object, #company] company specific info
+# @return [int] sum of all the top ups
 def sum_top_ups_for_company(users, company)
   (
-    (users[:emailed].nil? ? 0 : users[:emailed].length) + 
+    (users[:emailed].nil? ? 0 : users[:emailed].length) +
     (users[:not_emailed].nil? ? 0 : users[:not_emailed].length)
   ) * company['top_up']
 end
 
+# generate raw challenge data for our companies and users
+#
+# @param [Array, #users] users
+# @param [Array, #companies] companies
+# @return [Array] raw array of hashed data
+def generate_challenge_data(companies, users)
+  companies&.map { |company|
+    grouped_users_in_this_company = group_users_by_company(users, company)
+    {
+      'company' => company,
+      'emailed' => grouped_users_in_this_company[:emailed],
+      'not_emailed' => grouped_users_in_this_company[:not_emailed],
+      'total_top_ups' => sum_top_ups_for_company(grouped_users_in_this_company, company)
+    }
+  }
+end
+
+# rehsapes raw users data into our file specific format
+#
+# @param [Array, #users] users
+# @return [String] string in our desired format ready to write to our file
 def reshape_users_for_file(users)
-  shaped_data = ''
-
-  users&.each do |row|
-    shaped_data << "\t#{row['last_name']}, #{row['first_name']}, #{row['email']}\n"
-    shaped_data << "\t\tPrevious Token Balance #{row['tokens']}\n"
-    shaped_data << "\t\tNew Token Balance #{row['tokens_topped_up']}\n"
-  end
-
-  shaped_data
+  users&.map { |row|
+    "\t#{row['last_name']}, #{row['first_name']}, #{row['email']}\n"\
+    "\t\tPrevious Token Balance #{row['tokens']}\n"\
+    "\t\tNew Token Balance #{row['tokens_topped_up']}\n"\
+  }&.join('')
 end
 
+# rehsapes raw challenge data into our file specific format
+#
+# @param [Array, #companies] companies
+# @return [String] string in our desired format ready to write to our file
 def reshape_companies_for_file(companies)
-  shaped_data = ''
-
-  companies.each do |row|
-    shaped_data << "Company Id: #{row['company']['id']}\n"
-    shaped_data << "Company Name: #{row['company']['name']}\n"
-    shaped_data << "Users Emailed: \n"
-    shaped_data << reshape_users_for_file(row['emailed'])
-    shaped_data << "Users Not Emailed: \n"
-    shaped_data << reshape_users_for_file(row['not_emailed'])
-    shaped_data << "Total amount of top ups for #{row['company']['name']}: #{row['total_top_ups']} \n"
-    shaped_data << "\n"
-  end
-
-  shaped_data
+  companies&.map { |row|
+    "Company Id: #{row['company']['id']}\n"\
+    "Company Name: #{row['company']['name']}\n"\
+    "Users Emailed:\n"\
+    "#{reshape_users_for_file(row['emailed'])}"\
+    "Users Not Emailed:\n"\
+    "#{reshape_users_for_file(row['not_emailed'])}"\
+    "Total amount of top ups for #{row['company']['name']}: #{row['total_top_ups']}"\
+  }&.join("\n\n")
 end
 
-def output_file(data)
-  File.write('challenge.txt', data)
+# dump our shaped data to the file
+#
+# @param [String] data
+def output_text_file(data)
+  File.write('output.txt', data)
 end
 
 companies = order_companies_by_id(JSON.parse(File.read('companies.json')))
 users = order_users_by_last_name(JSON.parse(File.read('users.json')))
 
-challenge_data = companies.map { |company|
-  grouped_users_in_this_company = group_users_by_company(users, company)
-  puts grouped_users_in_this_company
-  {
-    'company' => company,
-    'emailed' => grouped_users_in_this_company[:emailed],
-    'not_emailed' => grouped_users_in_this_company[:not_emailed],
-    'total_top_ups' => sum_top_ups_for_company(grouped_users_in_this_company, company)
-  }
-}
-
+challenge_data = generate_challenge_data(companies, users)
 reshaped_challenge_data = reshape_companies_for_file(challenge_data)
 
-output_file(reshaped_challenge_data)
+output_text_file(reshaped_challenge_data)
